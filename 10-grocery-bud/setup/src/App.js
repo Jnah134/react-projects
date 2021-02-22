125822eb53a0f1cc20e436d91f9ce567966f6cb3
Redouane Jnah <redouanejnah2000@gmail.com>
2021-02-22 23:20:40 +0100

    working on grocery bud

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,69 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEdiiId] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      // display alert
+      showAlert(true, 'danger', 'Please enter the value');
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      showAlert(true, 'success', 'Item added to the list');
+      const newItems = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItems]);
+      setName(''); // I wanna clear the input after adding an item
+    }
+  };
+
+  const clearList = () => {
+    showAlert(true, 'danger', 'Empty list');
+    setList([]);
+  };
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg });
+  };
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'removed item');
+  };
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
+  );
 }
 
-export default App
+export default App;
