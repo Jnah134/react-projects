c8b1a1bd5e747785dd7209ea48f64eb3be21e296
Redouane Jnah <redouanejnah2000@gmail.com>
2021-03-11 22:10:25 +0100

    context API

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -2,23 +2,47 @@ import React, { useState, useRef, useEffect } from 'react';
 import { useGlobalContext } from './context';
 
 const Submenu = () => {
-  const { isSubmenuOpen, location } = useGlobalContext();
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext();
   const container = useRef(null);
 
+  const [colmuns, setColmuns] = useState('col-2');
+
   useEffect(() => {
+    setColmuns('col-2');
     const submenu = container.current;
     const { center, bottom } = location;
-    console.log(center);
     submenu.style.left = `${center}px`;
-    submenu.style.bottom = `${bottom}px`;
-  }, [location]);
+    submenu.style.top = `${bottom}px`;
+
+    if (links.length === 3) {
+      setColmuns('col-3');
+    }
+    if (links.length > 3) {
+      setColmuns('col-4');
+    }
+  }, [location, colmuns]);
 
   return (
     <aside
       className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}
       ref={container}
     >
-      submenu
+      <h4>{page}</h4>
+      <div className={`submenu-center ${colmuns} `}>
+        {links.map((link, index) => {
+          const { url, icon, label } = link;
+          return (
+            <a href={url} key={index}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
     </aside>
   );
 };
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -7,6 +7,7 @@ const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
   const [location, setLocation] = useState({});
+  const [page, setPage] = useState({ page: '', links: [] });
 
   const openSidebar = () => {
     setIsSidebarOpen(true);
@@ -16,6 +17,9 @@ const AppProvider = ({ children }) => {
     setIsSidebarOpen(false);
   };
   const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+
+    setPage(page);
     setLocation(coordinates);
     setIsSubmenuOpen(true);
   };
@@ -33,6 +37,7 @@ const AppProvider = ({ children }) => {
         closeSidebar,
         closeSubmenu,
         location,
+        page,
       }}
     >
       {children}
