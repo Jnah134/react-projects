b689ba8c022430dad2604a9994cd46e2b32de99d
Redouane Jnah <redouanejnah2000@gmail.com>
2021-03-14 12:08:57 +0100

    cocktail project

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere in est
+        ea rem aliquam sequi reiciendis culpa nostrum laudantium ducimus a,
+        officiis id et repellendus dolore explicabo excepturi voluptatum dolorem
+        sit? Beatae ullam ratione quo amet sequi velit facere dolores?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
